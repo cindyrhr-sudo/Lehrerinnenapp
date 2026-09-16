43c4ac44b3143f119c8b22b2a51001f408da87c6
cindyrhr-sudo <cindyrhr@gmail.com>
2026-09-16 09:18:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
 // weiterhin die alte, zwischengespeicherte Version!
 // =========================================================================
 
-const CACHE_VERSION = 'v14';
+const CACHE_VERSION = 'v15';
 const CACHE_NAME = `schulplaner-cache-${CACHE_VERSION}`;
 
 // Alle Dateien, die für den Offline-Betrieb nötig sind.
